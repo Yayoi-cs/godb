9b150e8751201dbe33d6aa277b29bc37d6a8510e
Yayoi-cs <hirogoshawk3249@gmail.com>
2025-01-08 16:42:59 +0900

    solving some reversing challenge.

--- main.go
+++ main.go
@@ -1,44 +1,109 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"godb/dbg"
 	"sync"
 	"time"
 )
 
-func analyze(wg *sync.WaitGroup, id int) {
+const (
+	charset = "abcdefghijklmnopqrstuvwxyz0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZ{}"
+	//charset = "at8"
+)
+
+type typeRes struct {
+	c      byte
+	status int
+}
+
+func analyze(wg *sync.WaitGroup, retChan chan typeRes, ctx context.Context, id int, flag string) {
 	defer wg.Done()
-	bin := "/home/tsuneki/dc/ctf/myctf/dbgerTest/dbgTest"
-	dbger, err := dbg.Run(bin, true)
-	if err != nil {
-		fmt.Println(err)
-		return
+	bin := "/home/tsuneki/dc/ctf/alpaca/r8/hidden"
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		dbger, err := dbg.Run(bin, true, flag)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		dbger.Wait()
+		_, err = dbger.Break(0x151c)
+		if err != nil {
+			fmt.Println(err)
+		}
+		dbger.Continue()
+		dbger.Wait()
+		rdi, err := dbger.GetRdi()
+		if err != nil {
+			fmt.Printf("[%x]%v\n", id, err)
+		}
+		rsi, err := dbger.GetRsi()
+		if err != nil {
+			fmt.Printf("[%x]%v\n", id, err)
+		}
+		//fmt.Printf("rdi: %x ,rsi: %x\n", rdi, rsi)
+		var i int
+		lflag := len(flag)
+		for i = 0; i < lflag+2; i++ {
+			rdiVal, err := dbger.GetMemory1(uintptr(rdi + uint64(i)))
+			if err != nil {
+				fmt.Printf("[%x]%v\n", id, err)
+			}
+			rsiVal, err := dbger.GetMemory1(uintptr(rsi + uint64(i)))
+			if err != nil {
+				fmt.Printf("[%x]%v\n", id, err)
+			}
+			//fmt.Printf("%x:[%x]%x ,[%x]%x\n", id, rdi+uint64(i), rdiVal, rsi+uint64(i), rsiVal)
+			if rdiVal != rsiVal || rdiVal == 0 || rsiVal == 0 {
+				break
+			}
+		}
+		retChan <- typeRes{flag[lflag-1], i}
+		dbger.Continue()
+		fmt.Printf("[%x]End analysis\n", id)
+	}()
+	select {
+	case <-ctx.Done():
+
+	case <-done:
 	}
-	dbger.Wait()
-	//fmt.Println(dbger.GetRegs())
-	_, err = dbger.Break(0x1169)
-	if err != nil {
-		fmt.Println(err)
-	}
-	dbger.Continue()
-	dbger.Wait()
-	fmt.Println(dbger.GetRegs())
-	dbger.Continue()
-	fmt.Printf("[%x]wait4child\n", id)
-	dbger.Wait()
-	fmt.Printf("[%x]End analysis\n", id)
 }
 
 func main() {
 	fmt.Println("[*]main")
-	var wg sync.WaitGroup
-	for i := 0; i < 3; i++ {
-		time.Sleep(20)
-		wg.Add(1)
-		go analyze(&wg, i)
+	flag := "Alpaca{"
+	for {
+		res := make(chan typeRes, len(charset))
+		var wg sync.WaitGroup
+		for i, c := range charset {
+			fmt.Println(flag + string(c))
+			time.Sleep(time.Millisecond * 25)
+			f := flag + string(c)
+			ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+			defer cancel()
+			wg.Add(1)
+			go analyze(&wg, res, ctx, i, f)
+		}
+		wg.Wait()
+		close(res)
+		var max typeRes = typeRes{
+			c:      byte(0),
+			status: 0,
+		}
+		for r := range res {
+			if r.status > max.status {
+				max.c = r.c
+				max.status = r.status
+			}
+		}
+		flag = flag + string(max.c)
+		if string(max.c) == "}" {
+			break
+		}
 	}
-	wg.Wait()
 	fmt.Println("[*]Shutdown godb")
 	return
 }
